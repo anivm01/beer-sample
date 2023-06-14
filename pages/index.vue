09a7bbaa15c20d6768580551ece191cc8cea3443
anivm01 <anivm.webdev@gmail.com>
2023-06-14 12:19:57 -0400

    add dry hopped ribbon

--- pages/index.vue
+++ pages/index.vue
@@ -78,6 +78,7 @@ const { data: beers } = await useFetch('https://api.punkapi.com/v2/beers?brewed_
         width: 100%;
         background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
         border: 4px solid black;
+        position: relative;
     }
     .image {
         object-fit: contain;
@@ -102,6 +103,49 @@ const { data: beers } = await useFetch('https://api.punkapi.com/v2/beers?brewed_
     .description {
         font-weight: 100;
     }
+    .dry{
+        width: 150px;
+        height: 150px;
+        overflow: hidden;
+        position: absolute;
+        top: -10px;
+        right: -10px;
+    }
+    
+    .dry::before,.dry::after {
+        position: absolute;
+        z-index: -1;
+        content: '';
+        display: block;
+        border: 5px solid #572677;
+        border-top-color: transparent;
+        border-right-color: transparent;
+    }
+
+    .dry::before {
+        top: 0;
+        left: 0;
+    }
+    .dry::after {
+        bottom: 0;
+        right: 0;
+    }
+    .dry_text {
+        position: absolute;
+        display: block;
+        width: 225px;
+        padding: 15px 0;
+        background-color: #833ab4;
+        box-shadow: 0 5px 10px rgba(0,0,0,.1);
+        color: #fff;
+        font: 700 18px/1 'Lato', sans-serif;
+        text-shadow: 0 1px 1px rgba(0,0,0,.2);
+        text-transform: uppercase;
+        text-align: center;
+        left: -25px;
+        top: 30px;
+        transform: rotate(45deg);
+    }
 
     @media (max-width: 767px) {
         .item {
@@ -120,12 +164,13 @@ const { data: beers } = await useFetch('https://api.punkapi.com/v2/beers?brewed_
                     <img class="image" :src=beer.image_url :alt="beer.name"/>
                 </div>
                 <div class="details">
+                    <div class="dry" v-if="beer.isDryHopped"><span class="dry_text">Dry Hopped</span></div>
                     <h2 class="name">{{ beer.name }}</h2>
                     <h3 class="tagline">{{ beer.tagline }}</h3>
                     <div><span class="label">ABV:</span><span>{{ beer.abv }}</span></div>
                     <div><span class="label">IBU:</span><span>{{ beer.ibu }}</span></div>
-                    <h4 v-if="beer.hasLactose">This item contains Lactose</h4>
                     <p class="description">{{ beer.description }}</p>
+                    <h4 v-if="beer.hasLactose">*This item contains Lactose</h4>
                 </div>
             </li>
         </ul>
